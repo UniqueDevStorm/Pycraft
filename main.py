491e581864e74a081913007faac59278760fd409
UniqueDevStorm <storm@uniquecode.team>
2021-02-15 09:13:39 +0900

    feat: class Voxel

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ app = Ursina()
 window.fps_counter.enable = False
 window.exit_button.visible = False
 
+class Voxel(Button):
+    def __init__(self, position=(0, 0, 0), texture='brick'):
+        super().__init__(
+            parent=scene,
+            position=position,
+            model='cube',
+            origin_y=0.5,
+            texture=texture,
+            color=color.color(0, 0, random.uniform(0.9, 1.0)),
+            scale=1.0
+        )
+
 app.run()
